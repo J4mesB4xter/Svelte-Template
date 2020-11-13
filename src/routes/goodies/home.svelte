d164b9c170e4b4bbb5af576e3bd09c69151c4fab
unknown <jbj1996@gmail.com>
2020-11-13 14:33:00 -0500

    new paragraph and its styling

--- src/routes/goodies/home.svelte
+++ src/routes/goodies/home.svelte
@@ -1,9 +1,17 @@
 <script>
-    let name = "John";
-    let src = 'iceking.jpg';
+    let name = "Friend";
+    let src = 'Images/iceking.jpg';
 </script>
 
+
+<style>
+    p {
+        color: grey;
+        font-family:'Times New Roman', Times, serif;
+        font-size: 2em;
+    }
+</style>
 <h1>Hello {name}!</h1>
 
-
-<img src={src} alt="{name} is yet to succeed">
+<p>Welcome to my Template. I hope you find something worth looking at eventually. It aint much yet, but it's honest work</p>
+<img {src} alt="{name} is yet to succeed">
