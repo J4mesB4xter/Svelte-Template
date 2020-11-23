95116165651b5a3addc76c56c29b0a6aef0e20b5
unknown <jbj1996@gmail.com>
2020-11-23 11:48:12 -0500

    added new page for playing with arrays and a num page from svelte tutorial to screw with at another point in time

---
+++ src/routes/goodies/arrays.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    let Cast = [" Galavant", " Richard", " Sid", " Isabella"]
+
+    function AddCast(str) {
+        Cast.push(str);
+        Cast = Cast;
+    }
+    let member = ''
+</script>
+
+<h1>The cast includes: </h1>
+<p>{Cast}</p>
+
+<input bind:value={member}>
+
+
+<button on:click= {AddCast(member)}></button>
---
+++ src/routes/goodies/num.svelte
@@ -0,0 +1,17 @@
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1];
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
+
+<p>I did not write this; for later dissection... when I'm in a different mood I want to reforge this page.</p>
