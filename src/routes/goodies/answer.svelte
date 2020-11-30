7b873a73d3e1a620d2d899c0fbe6bff9ab2a4cef
unknown <jbj1996@gmail.com>
2020-11-30 13:49:55 -0500

    added and implemented a default value for exported variable in nested file

--- src/routes/goodies/answer.svelte
+++ src/routes/goodies/answer.svelte
@@ -5,4 +5,7 @@
 
 <h1>What is the "Answer to the Ultimate Question of Life, the Universe, and Everything"</h1>
 
+<Nested2/>
+
+<p>10,000,000 years later...</p>
 <Nested2 answer={42}/>
--- src/routes/goodies/nested2.svelte
+++ src/routes/goodies/nested2.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let answer;
+    export let answer = "it will take me some time to consider return in 10,000,000 years";
 </script>
 
 <p>The answer to "Life, the Universe, and Everything" is: {answer}</p>
