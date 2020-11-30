1fcd3a9f3e02f743240f8247cbd1fedf326b1751
unknown <jbj1996@gmail.com>
2020-11-30 15:18:11 -0500

    created madlib around a strexcorp written job offer from the smiling god

---
+++ src/routes/goodies/madlib.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Nested3 from './nested3.svelte';
+
+    var pkg = {
+        job: '*',
+        category_of_measurement: '*',
+        emotion: '*',
+        noun: '*',
+        verb: '*',
+        adverb1: '*',
+        adverb2: '*',
+        adverb3: '*',
+        scary_verbed: '*',
+        important_noun: '*',
+        thing1: '*',
+        thing2: '*',
+        adjecting: '*',
+    }
+</script>
+job = <input bind:value={pkg.job}><br>
+metric = <input bind:value={pkg.category_of_measurement}><br>
+emotion = <input bind:value={pkg.emotion}><br>
+noun = <input bind:value={pkg.noun}><br>
+verb = <input bind:value={pkg.verb}><br>
+-ly adverb = <input bind:value={pkg.adverb1}><br>
+-ly adverb = <input bind:value={pkg.adverb2}><br>
+-ly adverb = <input bind:value={pkg.adverb3}><br>
+scary verb ending with -ed = <input bind:value={pkg.scary_verbed}><br>
+noun precious to you = <input bind:value={pkg.important_noun}><br>
+abstract thing = <input bind:value={pkg.thing1}><br>
+concrete thing = <input bind:value={pkg.thing2}><br>
+adject ending with -ing = <input bind:value={pkg.adjecting}><br>
+
+<Nested3 {...pkg}/>
---
+++ src/routes/goodies/nested3.svelte
@@ -0,0 +1,32 @@
+<script>
+export let job;
+export let category_of_measurement;
+export let emotion;
+export let noun;
+export let verb;
+export let adverb1;
+export let adverb2;
+export let adverb3;
+export let scary_verbed;
+export let important_noun;
+export let thing1;
+export let thing2;
+export let adjecting;
+</script>
+
+
+<p>
+Welcome to StrexCorp!<br><br>
+
+As a new {job}, you will be evaluated for personal {category_of_measurement} and {emotion}.
+Based on your own {category_of_measurement} you will receive daily {noun} reviews. Remember to always
+{verb} yourself to others, because if you fail to meet the standard of joy and {category_of_measurement}
+your coworkers do, you could be {adverb1}, {adverb2}, and {adverb3} {scary_verbed}.<br><br>
+
+Your {important_noun} ending will also mark the end of your job here at StrexCorp, so be careful not 
+to die, since your life has no meaning or {emotion} outside of your {category_of_measurement} here at StrexCorp. <br><br>
+
+{thing1} to your {thing2},<br>
+{adjecting} god <br>
+CEO
+</p>
