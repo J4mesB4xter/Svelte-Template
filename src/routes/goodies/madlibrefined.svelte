a6b36ebb31663d54e602a585f2955ef6069f57aa
unknown <jbj1996@gmail.com>
2020-12-02 18:20:58 -0500

    started refining madlib to move toward a generalized format:

---
+++ src/routes/goodies/madlibrefined.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Nested3 from './nested3.svelte';
+
+    var pkg = {
+        job: '',
+        category_of_measurement: '',
+        emotion: '',
+        noun: '',
+        verb: '',
+        adverb1: '',
+        adverb2: '',
+        adverb3: '',
+        scary_verbed: '',
+        important_noun: '',
+        thing1: '',
+        thing2: '',
+        adjecting: '',
+    };
+</script>
