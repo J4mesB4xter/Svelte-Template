43377684297588ec069538e9e88f599db3c0221e
unknown <jbj1996@gmail.com>
2020-12-03 17:36:00 -0500

    added first attempt at html function to replace verbose madlib

--- src/routes/goodies/madlibrefined.svelte
+++ src/routes/goodies/madlibrefined.svelte
@@ -1,19 +1,29 @@
 <script>
     import Nested3 from './nested3.svelte';
 
-    var words = {
-        job: '',
-        category_of_measurement: '',
-        emotion: '',
-        noun: '',
-        verb: '',
-        adverb1: '',
-        adverb2: '',
-        adverb3: '',
-        scary_verbed: '',
-        important_noun: '',
-        thing1: '',
-        thing2: '',
-        adjecting: '',
-    };
+    var words = [
+        {category: 'job', value: ''},
+        {category: 'metric', value: ''},
+        {category: 'emotion', value: ''},
+        {category: 'noun', value: ''},
+        {category: 'verb', value: ''},
+        {category: 'adverb1', value: ''},
+        {category: 'adverb2', value: ''},
+        {category: 'adverb3', value: ''},
+        {category: 'scary_verbed', value: ''},
+        {category: 'important_noun', value: ''},
+        {category: 'thing1', value: ''},
+        {category: 'thing2', value: ''},
+        {category: 'adjective ending with -ing', value: ''},
+];
 </script>
+
+<ul>
+    {#each words as word}
+    <li>
+        {word.value} == <input bind:value={word.value}>
+    </li>
+    {/each}
+</ul>
+
+<Nested3 {...words}/>
